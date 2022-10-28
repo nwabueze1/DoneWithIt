e2736e3135987f9e788f69e5318e68986715f025
Fidelis Okeke <padrefidelis111@gmail.com>
2022-10-28 09:59:18 +0100

    made small changes

--- app/hooks/useSentry.js
+++ app/hooks/useSentry.js
@@ -3,7 +3,7 @@ import * as Sentry from "sentry-expo";
 export const useSentry = () => {
   const initializeSentry = () =>
     Sentry.init({
-      dsn: "https://1190c8b838044d84bdffc0d95a80feb7@o449109.ingest.sentry.io/4504056902057984",
+      dsn: "you dsn here",
       enableInExpoDevelopment: true,
       debug: true, // If `true`, Sentry will try to print out useful debugging information if something goes wrong with sending the event. Set it to `false` in production
     });
